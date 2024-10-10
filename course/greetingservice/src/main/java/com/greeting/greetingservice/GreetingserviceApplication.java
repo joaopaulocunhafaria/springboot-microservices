c4153f78838ab547fb84be68f896485146d5e132
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-10-10 14:49:44 -0300

    edit configuration files

--- course/greetingservice/src/main/java/com/greeting/greetingservice/GreetingserviceApplication.java
+++ course/greetingservice/src/main/java/com/greeting/greetingservice/GreetingserviceApplication.java
@@ -1,4 +1,4 @@
-package com.greeting.greetingservice;
+     package com.greeting.greetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
