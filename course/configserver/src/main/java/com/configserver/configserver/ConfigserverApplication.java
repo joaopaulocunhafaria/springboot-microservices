cb6b21058ac24ade528b54edcdaec62650cd1159
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-10-15 17:09:33 -0300

    adding @RefreshScope annotation on configServer service

--- course/configserver/src/main/java/com/configserver/configserver/ConfigserverApplication.java
+++ course/configserver/src/main/java/com/configserver/configserver/ConfigserverApplication.java
@@ -3,8 +3,10 @@ package com.configserver.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 @EnableConfigServer
 public class ConfigserverApplication {
 
