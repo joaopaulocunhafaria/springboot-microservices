de4b31819d85a6abba85ecb04214a75a6c6fa5dc
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-10-15 17:37:42 -0300

    creating namingserver to apply eureka service register

---
+++ course/namingserver/src/test/java/com/microservices/namingserver/NamingserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
