bfe42d5e555dd0dc859acb613086bc9796f0f08d
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-10-16 15:18:39 -0300

    adding configuration file to api-gateway to be able distribuiting between URL`s

---
+++ course/api-gateway/src/main/java/com/microservices/api_gateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,34 @@
+package com.microservices.api_gateway.configuration;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+        .route(
+            p -> p.path("/get").uri("http://httpbin.org:80")
+            )
+        .route(
+            p -> p.path("/cambioservice/**").uri("lb://cambioserver") 
+        ).route(
+            p -> p.path("/bookservice/**").uri("lb://bookservice") 
+        )
+        .build()
+            
+            
+            ;
+    }
+
+}
--- course/bookservice/src/main/java/com/microservice/bookservice/controller/BookControler.java
+++ course/bookservice/src/main/java/com/microservice/bookservice/controller/BookControler.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.microservice.bookservice.model.Book;
 import com.microservice.bookservice.proxy.CambioProxy;
 import com.microservice.bookservice.repository.BookRepository;
 import com.microservice.bookservice.response.Cambio;
-
-import java.util.Date;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("bookservice")
@@ -35,12 +31,12 @@ public class BookControler {
             @PathVariable("currency") String currency) {
 
         Book book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
- 
+
         Cambio cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         String port = environment.getProperty("local.server.port");
 
-        book.setEnviroment(port + " FEIGN");
+        book.setEnviroment("BOOK port: " + port + " Cambio port: " + cambio.getEnvironment() + " FEIGN");
         book.setCurrency(currency);
         book.setPrice(cambio.getConvertedValue());
         return book;
