7ee0b7005780ee7c34ea4c8bddbe17b23c78d8bc
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-10-17 14:10:36 -0300

    adding fallback method on findBook function Contoroller

--- course/bookservice/src/main/java/com/microservice/bookservice/controller/BookControler.java
+++ course/bookservice/src/main/java/com/microservice/bookservice/controller/BookControler.java
@@ -1,11 +1,14 @@
 package com.microservice.bookservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.bookservice.model.Book;
 import com.microservice.bookservice.proxy.CambioProxy;
@@ -18,6 +21,8 @@ import io.github.resilience4j.retry.annotation.Retry;
 @RequestMapping("bookservice")
 public class BookControler {
 
+    private Logger logger = LoggerFactory.getLogger(BookControler.class);
+
     @Autowired
     private Environment environment;
 
@@ -29,10 +34,13 @@ public class BookControler {
 
     @GetMapping(value = "/{id}/{currency}")
 
-    @Retry(name = "findbook")
+    @Retry(name = "findbook", fallbackMethod = "returnDefaultBook")
     public Book findBook(
             @PathVariable("id") Long id,
             @PathVariable("currency") String currency) {
+        
+        //Forcando erro com URL invalida para testar fallbackmethod        
+        var response = new RestTemplate().getForEntity("http://localhost:8000/foobar", String.class);
 
         Book book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
 
@@ -43,6 +51,16 @@ public class BookControler {
         book.setEnviroment("BOOK port: " + port + " Cambio port: " + cambio.getEnvironment() + " FEIGN");
         book.setCurrency(currency);
         book.setPrice(cambio.getConvertedValue());
+        return book;
+    }
+
+    public Book returnDefaultBook(Exception e) {
+
+        logger.info("Returning default book ");
+
+        Long defaultID = 1L;
+
+        Book book = repository.findById(defaultID).orElseThrow(() -> new RuntimeException("Book not found"));
         return book;
     }
 
--- course/bookservice/src/main/java/com/microservice/bookservice/controller/FooBarControler.java
+++ course/bookservice/src/main/java/com/microservice/bookservice/controller/FooBarControler.java
@@ -9,21 +9,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
 
 @RestController
 @RequestMapping("bookservice")
 public class FooBarControler {
- 
+
     private Logger logger = LoggerFactory.getLogger(FooBarControler.class);
 
     @GetMapping("/foobar")
-    @Retry(name = "foobar")
+    @Retry(name = "foobar", fallbackMethod = "fallbackMethod")
     public String fooBar() {
         logger.info("Request to foobar is received!!");
         var response = new RestTemplate().getForEntity("http://localhost:8000/foobar", String.class);
 
         return response.getBody();
+    }
+
+    public String fallbackMethod() {
+        return "Droped in fallbackMethod";
     }
 
 }
